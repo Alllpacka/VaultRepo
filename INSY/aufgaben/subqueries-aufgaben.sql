81706884df04e3f6cf41e72289320df1a921f1c1
Alllpacka <90849663+Alllpacka@users.noreply.github.com>
2022-12-07 12:58:32 +0100

    gaming 4: final push

--- INSY/aufgaben/subqueries-aufgaben.sql
+++ INSY/aufgaben/subqueries-aufgaben.sql
@@ -1,13 +1,23 @@
-﻿
---1) Welches ist das teuerste Buch in der Datenbank?
+﻿--1) Welches ist das teuerste Buch in der Datenbank?
+
 select titel, verkaufspreis from buecher where verkaufspreis = (select max(verkaufspreis) from buecher);
+
 --2) Welches ist das billigste Buch in der Datenbank?
+
 select titel, verkaufspreis from buecher where verkaufspreis = (select min(verkaufspreis) from buecher);
---3) Lassen Sie sich alle Bücher ausgeben, deren Einkaufspreis über dem durchschnittlichen Einkaufspreis aller Bücher in der Datenbank liegt.          
+
+--3) Lassen Sie sich alle Bücher ausgeben, deren Einkaufspreis über dem durchschnittlichen Einkaufspreis aller Bücher in der Datenbank liegt.   
+
 select titel from buecher where einkaufspreis > (select avg(einkaufspreis) from buecher);
+
 --4) Lassen Sie sich alle Bücher ausgeben, deren Einkaufspreis über dem durchschnittlichen Einkaufspreis der Thriller liegt.
+
 select distinct titel from buecher where einkaufspreis > (select avg(einkaufspreis) from buecher join buecher_has_sparten, sparten where buecher_has_sparten.sparten_sparten_id = sparten.sparten_id and buecher_has_sparten.buecher_buecher_id = buecher.buecher_id and sparten.sparten_id = 3);
+
 --5) Lassen Sie sich alle Thriller ausgeben, deren Einkaufspreis über dem durchschnittlichen Einkaufspreis der Thriller liegt.
+
 select distinct titel from buecher join buecher_has_sparten, sparten where einkaufspreis > (select avg(einkaufspreis) from buecher) and buecher_has_sparten.sparten_sparten_id = sparten.sparten_id and sparten.sparten_id = 3;
+
 --6) Lassen Sie sich alle Bücher ausgeben, bei denen der Gewinn überdurchschnittlich ist; bei der Berechnung des Gewinndurchschnitts berücksichtigen Sie NICHT das Buch mit der id 22.
+
 select titel, sparten_id from buecher, sparten where (verkaufspreis-einkaufspreis) < (select avg(verkaufspreis-einkaufspreis) from buecher where buecher_id != 22);
