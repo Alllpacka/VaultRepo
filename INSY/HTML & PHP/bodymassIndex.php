654cc4e9cd4401b219e459064dd75281e06ed1e1
Alllpacka <90849663+Alllpacka@users.noreply.github.com>
2023-01-18 12:00:35 +0100

    vault backup: 2023-01-18 12:00:35

--- INSY/HTML & PHP/bodymassIndex.php
+++ INSY/HTML & PHP/bodymassIndex.php
@@ -8,11 +8,15 @@
         $alt = 16;
         $gro = 170;
         $gew = 75;
+
+        $bmi = ($alt+$gew)/exp($gro);
+        
         echo "<h1>Bodymass-Index</h1>";
         echo "<br><br>";
         echo "Alter: $alt<br>";
         echo "Größe: $gro cm<br>";
-        echo "Gewicht: $gew kg<br>"
+        echo "Gewicht: $gew kg<br>";
+        echo "BMI: $bmi"
         ?>
     </body>
 </html>
