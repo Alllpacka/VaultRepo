ff066eab168b8dff248462419c02733255574992
Alllpacka <90849663+Alllpacka@users.noreply.github.com>
2023-01-20 09:34:25 +0100

    vault backup: 2023-01-20 09:34:25

--- INSY/HTML & PHP/bodymassIndex.php
+++ INSY/HTML & PHP/bodymassIndex.php
@@ -9,6 +9,12 @@
             <input type="text" id="falter" name="falter"><br>
             <label for="fgroese">Größe in m</label><br>
             <input type="text" id="fgroese" name="fgroese"><br>
+            <label for="fgewicht">Gewicht in kg</label><br>
+            <input type="text" id="fgewicht" name="fgewicht"><br><br>
+            <input type="radio" id="fmanlich" name="fmanlich"><br>
+            <label for="fmanlich">Mänlich</label><br>
+            <input type="radio" id="fweiblich" name="fweiblich"><br>
+            <label for="fweiblich">Weiblich</label>
         </form>
     </body>
 </html>
